e4643ea7526ebfc1ba5d53e71d9e98dfed0de65b
Friday79 <fuwuigbe@gmail.com>
2025-03-11 16:51:34 +0000

    products admin added

--- products/models.py
+++ products/models.py
@@ -25,30 +25,4 @@ class Product(models.Model):
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
-        return self.name
-
-from django.contrib import admin
-from .models import Product, Category
-
-# Register your models here.
-
-class ProductAdmin(admin.ModelAdmin):
-    list_display = (
-        'sku',
-        'name',
-        'category',
-        'price',
-        'rating',
-        'image',
-    )
-
-    ordering = ('sku',)
-
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = (
-        'friendly_name',
-        'name',
-    )
-
-admin.site.register(Product, ProductAdmin)
-admin.site.register(Category, CategoryAdmin)       
+        return self.name       
