3a1cb98d7eaa45ef8a8b14c4b360f9fad2ef1352
Friday79 <fuwuigbe@gmail.com>
2025-03-30 19:20:36 +0000

    fix secret key issues

--- my_collections/settings.py
+++ my_collections/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('Justinafr1!')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://mycollections-379ea5dbbc8f.herokuapp.com/']
 
