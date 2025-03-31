16ec3e5c2646c39792744bb16841ccf016c3db52
Friday79 <fuwuigbe@gmail.com>
2025-03-31 15:15:39 +0000

    secret_key added to settings.py

--- my_collections/settings.py
+++ my_collections/settings.py
@@ -25,12 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('Justinafr1!')
+SECRET_KEY = 'Justinafr1!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://mycollections-379ea5dbbc8f.herokuapp.com/']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1',]
 
 
 # Application definition
