85313712dd0b421483fbfaf51185b41fe1bcaf16
Friday79 <fuwuigbe@gmail.com>
2025-04-01 10:34:36 +0000

    static root and storage added

--- my_collections/settings.py
+++ my_collections/settings.py
@@ -195,6 +195,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+# WhiteNoise Storage
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
