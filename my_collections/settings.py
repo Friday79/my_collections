a939eee90ec751755be99b2296433bd5fad04ed9
Friday79 <fuwuigbe@gmail.com>
2025-06-14 19:05:15 +0000

    update settings with static

--- my_collections/settings.py
+++ my_collections/settings.py
@@ -217,32 +217,59 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATICFILES_LOCATION = 'static'
+MEDIAFILES_LOCATION = 'media'
+
 if 'USE_AWS' in os.environ:
-    # Cache control
+
     AWS_S3_OBJECT_PARAMETERS = {
         'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
         'CacheControl': 'max-age=94608000',
     }
-    
-    # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'mycollections1' # change this to your AWS bucket name
+    AWS_STORAGE_BUCKET_NAME = 'mycollections1'
     AWS_S3_REGION_NAME = 'eu-north-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
-    # Static and media files
-    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
-    STATICFILES_LOCATION = 'static'
-    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
-    MEDIAFILES_LOCATION = 'media'
 
-    # Override static and media URLs in production
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    
+
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 else:
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'    
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+#if 'USE_AWS' in os.environ:
+    # Cache control
+#    AWS_S3_OBJECT_PARAMETERS = {
+#        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+#        'CacheControl': 'max-age=94608000',
+#    }
+    
+    # Bucket Config
+#    AWS_STORAGE_BUCKET_NAME = 'mycollections1' # change this to your AWS bucket name
+#    AWS_S3_REGION_NAME = 'eu-north-1'
+#    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+#    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+#    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+    # Static and media files
+#    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+#    STATICFILES_LOCATION = 'static'
+#    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+#    MEDIAFILES_LOCATION = 'media'
+
+    # Override static and media URLs in production
+#    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+#    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+
+#else:
+#    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'    
 
 #CLOUDINARY_URL = os.getenv('CLOUDINARY_URL')
 #DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
