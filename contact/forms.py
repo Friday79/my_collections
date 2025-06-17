23d2814e15c00e9504115eae3b92f8f19deabc25
Friday79 <fuwuigbe@gmail.com>
2025-06-17 11:16:36 +0000

    contact html update

--- contact/forms.py
+++ contact/forms.py
@@ -4,4 +4,3 @@ class ContactForm(forms.Form):
     name = forms.CharField(max_length=100, required=True, label='Your Name')
     email = forms.EmailField(required=True, label='Your Email')
     message = forms.CharField(widget=forms.Textarea, required=True, label='Your Message')
-    
