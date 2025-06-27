8726916f2fcac0622309f610a0ab6b0ed5785de3
Friday79 <fuwuigbe@gmail.com>
2025-06-27 18:31:35 +0000

    aws configured

--- my_collections/settings.py
+++ my_collections/settings.py
@@ -258,7 +258,7 @@ if 'USE_AWS' in os.environ:
     }
     
     # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'mycollections-bucket' # change this to your AWS bucket name
+    AWS_STORAGE_BUCKET_NAME = 'mycollections1' # change this to your AWS bucket name
     AWS_S3_REGION_NAME = 'eu-north-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
