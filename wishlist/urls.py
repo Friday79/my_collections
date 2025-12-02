688e2d62a9bd63f17b2c46723d396437962bb493
Friday79 <fuwuigbe@gmail.com>
2025-12-02 21:44:43 +0000

    ability to move item from wishlist to bag added

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.wishlist_view, name="wishlist"),
     path('add/<int:product_id>/', views.add_to_wishlist, name='add_to_wishlist'),
     path('remove/<int:product_id>/', views.remove_from_wishlist, name='remove_from_wishlist'),
+    path("move_to_bag/<int:product_id>/", views.move_to_bag, name="move_to_bag"),
 ]
+
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from products.models import Product
-from .models import Wishlist 
+from .models import Wishlist
+from django.contrib import messages 
 
 # Create your views here.
 @login_required
@@ -20,3 +21,22 @@ def remove_from_wishlist(request, product_id):
     product = get_object_or_404(Product, id=product_id)
     Wishlist.objects.filter(user=request.user, product=product).delete()
     return redirect("wishlist")
+
+@login_required
+def move_to_bag(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    bag = request.session.get('bag', {})
+
+    # For products without size
+    if str(product_id) in bag:
+        bag[str(product_id)] += 1
+    else:
+        bag[str(product_id)] = 1
+
+    # Save back to session
+    request.session['bag'] = bag
+
+    # Remove from wishlist
+    Wishlist.objects.filter(user=request.user, product=product).delete()
+
+    return redirect('view_bag')
