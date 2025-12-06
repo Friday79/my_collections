8e1a0a1f4bf05185f70248ee9d9a1a927c497293
Friday79 <fuwuigbe@gmail.com>
2025-12-06 11:56:26 +0000

    added sale funtionality for user to buy product on sale

--- products/models.py
+++ products/models.py
@@ -1,8 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from cloudinary.models import CloudinaryField
-
-
 
 
 class Category(models.Model):
@@ -11,7 +8,8 @@ class Category(models.Model):
         verbose_name_plural = 'Categories'
         
     name = models.CharField(max_length=254)
-    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+    friendly_name = models.CharField(
+        max_length=254, null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -21,26 +19,43 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey(
+        'Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
     has_sizes = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    sale_price = models.DecimalField(
+        max_digits=6, decimal_places=2, null=True, blank=True)
+    rating = models.DecimalField(
+        max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(upload_to='products/', blank=True, null=True) 
 
     def __str__(self):
-        return self.name       
+        return self.name 
+
+    @property
+    def is_on_sale(self):
+        return self.sale_price is not None and self.sale_price < self.price
+
+    @property
+    def discount_percentage(self):
+        if self.is_on_sale:
+            return round(100 - (self.sale_price / self.price * 100))
+        return 0      
 
 
 class Review(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="reviews")
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name="reviews")
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    rating = models.IntegerField(choices=[(i, i) for i in range(1, 6)], default=5)  
+    rating = models.IntegerField(
+        choices=[(i, i) for i in range(1, 6)], default=5)  
     comment = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f'Review by {self.user} for {self.product}'
+    
